0bcb478b29259a77e55d448c0c891c5cd73bd698
Guilherme Oliveira <guilherme.061@gmail.com>
2019-06-11 16:12:07 -0400

    testing pixel perfect on sprite renderer unity 2019.2b

---
+++ JumpyJumpGame/Assets/Scripts/TESTING.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TESTING : MonoBehaviour {
+    Vector3 m_originalPosition;
+
+    void Start() {
+        m_originalPosition = transform.position;
+    }
+
+    void Update() {
+        transform.position = m_originalPosition + new Vector3(Mathf.Sin(Time.time), Mathf.Cos(Time.time), 0f);
+    }
+}
